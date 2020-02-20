e420ddd658b930a78cb90664f2b34dd8f592d239
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 13:27:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,6 +10,8 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,int)
   if(valid_move? == true && (board[int-1]!= "X" ||board[int-1]!= "O") )
-
+    return true
+  else
+    return false
   end
 end
